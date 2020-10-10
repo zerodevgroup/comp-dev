4dfd38edc28601b46a9c8de5dd24e90a0c491be8
vortex7 <sean.wright.vortex@gmail.com>
2020-10-10 14:24:30 -0400

    added: src/components/team-member-manager.js

--- comp-dev-ui/src/pages/team-members.js
+++ comp-dev-ui/src/pages/team-members.js
@@ -3,6 +3,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+import TeamMemberManager from "../components/team-member-manager"
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -20,6 +22,7 @@ export default function TeamMembers() {
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
           Team Members
+          <TeamMemberManager />
         </Typography>
       </div>
     </Container>
