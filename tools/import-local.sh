a44b6406c0e48ce084f3687bda149219c7e0338e
vortex7 <sean.wright.vortex@gmail.com>
2020-10-26 09:30:43 -0400

    deleted:    docs/COMP-DEV-API.md deleted:    docs/COMP-DEV-CLOUD-SETUP.md deleted:    docs/COMP-DEV-DATA.md deleted:    docs/COMP-DEV-MONGO.md deleted:    docs/COMP-DEV-VM-SETUP.md deleted:    docs/COMP-DEV-WEB.md deleted:    docs/README.md added: onfig/machines/local/ tools/import-local.sh

---
+++ tools/import-local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <source data directory>"
+  exit -1
+fi
+
+DATA_DIR=$1
+
+if [ ! -d "$DATA_DIR" ]; then
+  echo "$DATA_DIR not found"
+  exit -1
+fi
+
+cd $DATA_DIR
+
+#######################
+# team-member data
+#######################
+## team-members
+if [ -f "team-members.json" ]; then
+  echo team-members
+  curl --silent -X POST -H 'accept: application/json' -H 'content-type: application/json' --data-binary "@./team-members.json" http://localhost/comp-dev-api/create/teammembers > /dev/null
+else
+  echo "team-members.json not found"
+fi
+
