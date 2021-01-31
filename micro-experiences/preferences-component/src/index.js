d8fe450c0c3ed1e497a029f7819b0514de8dae48
zerodevgroup <admin@zerodevgroup.com>
2021-01-31 18:44:43 -0500

    added: micro-experiences/ react-apps/

---
+++ micro-experiences/preferences-component/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Component() {
+  return <div>Our package</div>
+}
+
+export default Component
---
+++ react-apps/preferences-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+import PreferencesComponent from 'preferences-component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <PreferencesComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
